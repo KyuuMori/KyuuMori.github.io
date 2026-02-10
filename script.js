cbdb38c7b692eef91661821c30120d21966ab05f
KyuuMori <93170641+KyuuMori@users.noreply.github.com>
2026-02-10 10:28:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const message = document.getElementById("secretMessage");
 
 button.addEventListener("click", () => {
   message.classList.toggle("show");
-  button.textContent = "My heart is yours 💖";
+  button.textContent = "You got mail! 💖";
 });
